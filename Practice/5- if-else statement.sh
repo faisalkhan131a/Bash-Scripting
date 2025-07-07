bbcb0618f5d6f20647bacee5d42222c00ed0bd5d
Faisal Khan <76738528+faisalkhan131a@users.noreply.github.com>
2025-07-07 20:02:40 +0600

    Update 5- if-else statement.sh

--- Practice/5- if-else statement.sh
+++ Practice/5- if-else statement.sh
@@ -4,7 +4,7 @@
 # you can give any other value than 'user' and check the ouput and modify the script accordingly.
 
 
-#in the first if block, we have a condition and what will happen if the condition is true.
+# in the first if block, we have a condition and what will happen if the condition is true.
 # what does ${1,,} do? => ${1} passes the user input to the first argument $1 and ${1,,} converts the value of $1 to small letters. Like USER to user. This is just to simplify reading user inputs.
 
 if [ ${1,,} = user ]; then
