763dea9bdc8f7c67a76c100ff6651bf1228b569b
Faisal Khan <76738528+faisalkhan131a@users.noreply.github.com>
2025-07-07 20:09:01 +0600

    Update 7- using a function.sh

--- Practice/7- using a function.sh
+++ Practice/7- using a function.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-#using a function in bash. Execute the file using- bash filename.bash . No need to pass any value while executing.
+# function in bash. 
+# execute the file using- bash filename.bash . No need to pass any value while executing for this file.
 
-#The function Showuptime() will show the uptime and time since last reboot in output. 
-#We have used one variable to store the uptime value ($uptime) and another for last reboot time value ($since).
+# function Showuptime() will show the uptime and time since last reboot in output. 
+# we are storing the uptime value in $uptime and value of last reboot time in $since.
 
 Showuptime(){
 up=$(uptime -p | cut -c4- )
 since=$(uptime -s)
 
-#This is a EOF marker. EOF is used to print a number of lines instead of typing 'echo' for every line.
-#There is a starting EOF and an ending EOF. Bash will print all the text in between.
+# this is called an EOF marker. EOF is used to print  multiple lines instead of typing 'echo' for every line.
+# there is a starting EOF and an ending EOF. Bash will print all the text in between.
 
 cat << EOF
 
@@ -22,7 +23,7 @@ It has been running since ${since}
 EOF
 }
 
-#Here we have called the funtion.
+# In the end, we have called the funtion for execution.
 
 Showuptime
 
