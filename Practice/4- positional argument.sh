d8b6934eb87345285431be5f5ea1c52a900c0727
Faisal Khan <76738528+faisalkhan131a@users.noreply.github.com>
2025-07-07 18:41:15 +0600

    Update 4- positional argument.sh

--- Practice/4- positional argument.sh
+++ Practice/4- positional argument.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# $1 and $2 is our positional arguments and the values for those arguments will be passed while executing the bash file.
-# run the file like this -> bash file_name.sh Counter Strike 
+# $1 and $2 is our positional arguments and the value for those arguments will be passed while executing the bash file.
+# run the file like this -> bash filename.sh Counter Strike 
 # you can use any value instead of 'Counter Strike', just make sure it contains 2 words as we have declared 2 arguments
 # here 'Counter' and 'Strike' is the value of the argument 1 and 2. Lastly we have thee cho statement to print this
 
